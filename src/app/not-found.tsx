60360a516d5b12cd8dc773cd4571fab4d573e6f1
tdarrow7 <tdarrow7@users.noreply.github.com>
2024-06-10 17:32:33 -0700

    added 404 page and favicon

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+        <h1 className='text-center'>Looks like this page is burnt</h1>
+    </div>
+  )
+}
--- src/app/products/[product]/page.tsx
+++ src/app/products/[product]/page.tsx
@@ -1,9 +1,7 @@
 'use client'
 import { usePathname } from 'next/navigation'
-// import { useRouter } from "next/router";
 
 export default function ProductPage() {
-    // const router = useRouter();
     return (
         <main>Product Page: {usePathname()}</main>
     );
