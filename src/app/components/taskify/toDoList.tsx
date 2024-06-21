b08b24cd397bfc5f99a6ee59c64ff881875eb49b
tdarrow7 <tdarrow7@users.noreply.github.com>
2024-06-21 08:24:22 -0700

    added additional functionality to taskify lists

--- src/app/components/taskify/toDoList.tsx
+++ src/app/components/taskify/toDoList.tsx
@@ -14,12 +14,15 @@ const ToDoList = ({isDone}: TodoListProps) => {
   const newTodos = todos.filter((todo) => todo.isDone === isDone)
   return (
     <div className={`min-h-56 ${isDone ? 'bg-green-950 rounded-l-md' : 'bg-sky-950 rounded-r-md'}`}>
-      <ul className={`todos p-4 mt-4 w-full max-w-7xl grid grid-cols-3 gap-4 container px-4`}>
-          {newTodos.map((toDo) => (
-            <li key={toDo.id}>
+      <ul className={`todos p-4 mt-4 w-full max-w-7xl grid grid-cols-${newTodos.length > 0 ? '3 gap-4' : '1'} container px-4`}>
+        {newTodos.length > 0 ? 
+          newTodos.map((toDo) => (
+              <li key={toDo.id}>
               <ToDoItem todo={toDo}></ToDoItem>
-            </li>
-          ))}
+              </li>
+            ))
+            : <li><strong className='text-3xl'>{isDone ? 'Completed' : 'Pending'} ToDo List is Empty</strong></li>
+        }
       </ul>
     </div>
   )
